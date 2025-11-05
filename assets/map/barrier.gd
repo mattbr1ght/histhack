b83f6efc9ee9b00930404ce01cd84afcfbf7b9ee
Mateusz Wroński <78876597+mattbr1ght@users.noreply.github.com>
2025-11-05 13:16:14 +0100

    find dama z gronostajem story and minigame on saski palace

---
+++ assets/map/barrier.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await StoryManager.saski_after_time_travel
+	self.queue_free()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
